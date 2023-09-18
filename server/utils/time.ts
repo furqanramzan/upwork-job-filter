ea76a6ca4efcf7bd8b02042d0db8e449d3673402
Muhammad Furqan <furqanramzan271996@gmail.com>
2023-09-18 15:50:23 +0500

    fix(time): add plural times in get time form string

--- server/utils/time.ts
+++ server/utils/time.ts
@@ -8,14 +8,21 @@ export function getTimeFromString(timeString: string): Date {
   const unitMapping: { [key: string]: number } = {
     sec: 1000,
     second: 1000,
+    seconds: 1000,
     min: 60000,
     minute: 60000,
+    minutes: 60000,
     hr: 3600000,
     hour: 3600000,
+    hours: 3600000,
     day: 86400000,
+    days: 86400000,
     week: 604800000,
+    weeks: 604800000,
     month: 2592000000, // Assuming 30 days per month
+    months: 2592000000,
     year: 31536000000, // Assuming 365 days per year
+    years: 31536000000,
   };
 
   // Calculate the time by subtracting the milliseconds from the current date and time
