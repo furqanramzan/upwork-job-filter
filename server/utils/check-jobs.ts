16d8a2ffab4e655a93e9b5dc7e7480d48010007e
Muhammad Furqan <furqanramzan271996@gmail.com>
2023-09-27 14:31:18 +0500

    fix(checkjob): add user agent in puppeteer

--- server/utils/check-jobs.ts
+++ server/utils/check-jobs.ts
@@ -23,6 +23,9 @@ export async function checkJobs() {
   }
   const browser = await launch(options);
   const page = await browser.newPage();
+  await page.setUserAgent(
+    'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36',
+  );
 
   let success = true;
   try {
--- server/utils/constants.ts
+++ server/utils/constants.ts
@@ -25,6 +25,8 @@ export const relevantWords = [
   'reactjs',
   'react.js',
   'react',
+  'trpc',
+  'prisma',
 ];
 export const irrelevantWords = [
   'webflow',
